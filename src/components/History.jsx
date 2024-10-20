23ec5a83809a27d19c78635bb907724662fb7021
Sushnata Sarkar <coolsushnata@gmail.com>
2024-10-20 06:20:28 +0530

    minor changes

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,14 +1,25 @@
 import * as React from "react";
 import JsonTable from "./JsonTable";
+import { useNavigate } from "react-router-dom";
 
 export default () => {
 
+    const navigate = useNavigate();
+
     const gameHistory = JSON.parse(localStorage.getItem("gameHistory")) || []
+
+    const clearHistory = () => {
+        console.log("Cleaning history . . .");
+        localStorage.removeItem("gameHistory");
+        console.log(localStorage);
+        console.log("Done");
+        navigate("/")
+    }
 
     return (
         <div className="pad">
-            <h2>History</h2>
-            <JsonTable objarr={gameHistory}/>
+            <h2>History <button onClick={clearHistory}>Clear</button></h2>
+            <JsonTable objarr={gameHistory} />
         </div>
     )
 }
--- src/components/JsonTable.jsx
+++ src/components/JsonTable.jsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 
-import test from '../test.json';
-
 export default ({ objarr }) => {
     //objarr = objarr ?? test
     // var objkeys = objarr ? Object.keys(objarr[0]) : [];            
@@ -21,14 +19,21 @@ export default ({ objarr }) => {
     //objarr = JSON.stringify(objarr);
 
     console.log(objarr);
+    var objkeys = Object.keys(objarr) || []
+
+    /* {objkeys.map((item,index)=>(
+                        <th key={index}>{item}</th>
+                    ))} */
 
     return (
         <table>
             <thead>
-                <tr><th>score</th><th>level</th><th>time_taken</th><th>recorded_at</th></tr>
+                <tr>
+                    <th>score</th><th>level</th><th>time_taken</th><th>recorded_at</th>
+                </tr>
             </thead>
             <tbody>
-                {objarr.map((item,index)=>(
+                {objarr.map((item, index) => (
                     <tr key={index}>
                         <td>{item.score}</td>
                         <td>{item.level}</td>
