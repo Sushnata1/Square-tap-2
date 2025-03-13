6157238e49d7f00deae18d8c67d2b95171de0210
Sushnata Sarkar <coolsushnata@gmail.com>
2025-03-13 07:56:19 +0530

    added gameplay video

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -1,15 +1,18 @@
 import * as React from "react";
-import {images} from "../constants.js"
+import { images } from "../constants.js"
 import Image from "../components/Image.jsx"
 
 export default () => {
     return (
         <>
             <div className="pad">
-                <h2>Help</h2>
-                <p>The aim of this game is to hover (for PCs) on the coloured squares (like '<Image src={images.good} typ="goodsquare" size="20" />' ) Or tap (for mobile devices) on them to get 1 point.
-                    If a white square (like '<Image src={images.bad} size="20" />' ) is tapped or hovered, the game is over. Press on <span className="name">dark red box</span> to
-                    change your name. If the history size becomes too large, Press on <span className="button">CLEAR</span> on the game over page.</p>
+                <h2>Help cum Gameplay</h2>
+                <iframe width="315" height="560"
+                    src="https://youtube.com/embed/yzh8zmVv6UI?si=YfBKo7K6UB0Ee9Fq"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen />
             </div>
         </>
     )
