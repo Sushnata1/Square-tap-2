77016f139f1449b8ef661ba1c6f8d94ee256af4e
Sushnata Sarkar <coolsushnata@gmail.com>
2025-03-15 06:39:28 +0530

    minor cosmetic changes

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -1,18 +1,20 @@
 import * as React from "react";
-import { images } from "../constants.js"
-import Image from "../components/Image.jsx"
+
+import './Help.scss'
 
 export default () => {
     return (
         <>
             <div className="pad">
                 <h2>Help cum Gameplay</h2>
-                <iframe width="315" height="560"
-                    src="https://youtube.com/embed/yzh8zmVv6UI?si=YfBKo7K6UB0Ee9Fq"
-                    title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowfullscreen />
+                <div>
+                    <iframe width="315" height="560"
+                        src="https://youtube.com/embed/yzh8zmVv6UI?si=YfBKo7K6UB0Ee9Fq"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen />
+                </div>
             </div>
         </>
     )
