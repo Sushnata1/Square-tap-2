aff305372e91d3f5d5c896b1e6740ab814ebe121
Sushnata Sarkar <coolsushnata@gmail.com>
2026-05-28 19:07:39 +0530

    'classic'

--- src/constants.js
+++ src/constants.js
@@ -1,44 +1,44 @@
-export const theme = "biswakarma_puja"
+export const theme = "biswakarma_puja";
 
 export const images = {
-    "bad": `./asur.jpeg`,
-    "good": `./durga.jpeg`
+    "bad": `./bomb.png`,
+    "good": `./box.jpg`
 }
 
 export const sounds = {
-    "good": new Audio("./good_sound.wav"),
-    "bad": new Audio("./bad_sound.wav")
-}
+  good: new Audio("./good_sound.wav"),
+  bad: new Audio("./bad_sound.wav"),
+};
 
 export const levels = {
-    "EASY": {
-        size: 100,
-        delay: 140,
-        number: 5
-    },
-    "MEDIUM": {
-        size: 80,
-        delay: 100,
-        number: 10
-    },
-    "HARD": {
-        size: 50,
-        delay: 80,
-        number: 20
-    },
-}
+  EASY: {
+    size: 100,
+    delay: 140,
+    number: 5,
+  },
+  MEDIUM: {
+    size: 80,
+    delay: 100,
+    number: 10,
+  },
+  HARD: {
+    size: 50,
+    delay: 80,
+    number: 20,
+  },
+};
 
 export const credits = {
-    "Development": {
-        "Sushnata Sarkar": {
-            "Github": "https://github.com/Sushnata1",
-            "LinkedIn": "https://www.linkedin.com/in/sushnata-sarkar-5a9752170/"
-        },
+  Development: {
+    "Sushnata Sarkar": {
+      Github: "https://github.com/Sushnata1",
+      LinkedIn: "https://www.linkedin.com/in/sushnata-sarkar-5a9752170/",
     },
-    "Graphics": {
-        "Sayantanee Sarkar": {
-            "Github": "https://github.com/sayariya",
-            "LinkedIn": "https://www.linkedin.com/in/sayantanee-sarkar-474766227/"
-        },
-    }
-}
+  },
+  Graphics: {
+    "Sayantanee Sarkar": {
+      Github: "https://github.com/sayariya",
+      LinkedIn: "https://www.linkedin.com/in/sayantanee-sarkar-474766227/",
+    },
+  },
+};
